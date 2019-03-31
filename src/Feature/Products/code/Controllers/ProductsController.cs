3fc81930ff35065ef6bd23b2ff0bb7679c68f875
Suhas Bharadwaj <suhas.five@gmail.com>
2019-04-01 01:48:56 +0530

    Added initial projects and views. First commit.

---
+++ src/Feature/Products/code/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using SitecoreCITest.Feature.Products.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace code.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var index = 0;
+            var model = new CatalogViewModel();
+            model.AddProduct(new Product { Id = ++index, Name = "Galaxy S10" });
+            model.AddProduct(new Product { Id = ++index, Name = "Google Pixel 3" });
+            model.AddProduct(new Product { Id = ++index, Name = "Galaxy Note 9" });
+            model.AddProduct(new Product { Id = ++index, Name = "OnePlus 7" });
+
+            return View("~/Views/Feature/Products/Catalog.cshtml", model);
+        }
+    }
+}
---
+++ src/Feature/Products/code/Models/CatalogViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SitecoreCITest.Feature.Products.Models
+{
+    public class CatalogViewModel
+    {
+        public CatalogViewModel()
+        {
+            this.Products = new List<Product>();
+        }
+
+        public string Id { get; set; }
+
+        public List<Product> Products { get; set; }
+
+        public void AddProduct(Product product)
+        {
+            this.Products.Add(product);
+        }
+    }
+}
